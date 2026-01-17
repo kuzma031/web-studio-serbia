198709b21ebd169e6d0c94dc1e77cf479524d0c0
djordje-voiced <djordje@voiced.com>
2026-01-18 00:32:44 +0100

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import Script from "next/script";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,12 +11,26 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Component {...pageProps} />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-915PQ1ZLTF"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-915PQ1ZLTF');
+        `}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <Component {...pageProps} />
+        </TooltipProvider>
+      </QueryClientProvider>
+    </>
   );
 }
